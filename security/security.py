ac4f3985f8534fc4009b12957f7bcecab63ffa76
MigasEustaquio <31831761+MigasEustaquio@users.noreply.github.com>
2020-12-03 16:05:04 -0300

    Update security.py

--- security/security.py
+++ security/security.py
@@ -1,1 +1,10 @@
-hello world
+import facerecognizer
+
+if __name__ == "__main__":
+  
+	name = facerecognizer.recog()
+
+	if name == "unknown":
+		print("\nSecurity Breach\n")
+	else:
+		print("\n" + name + "\n")
